6a575f3ab4dd34579dc6193ae69738c8fddfbfd4
fengb <contact@fengb.info>
2013-07-31 22:24:27 -0500

    ReadOnly from load

--- lib/active_record_immutable.rb
+++ lib/active_record_immutable.rb
@@ -1,5 +1,13 @@
 require "active_record_immutable/version"
 
 module ActiveRecordImmutable
-  # Your code goes here...
+  def self.extended(base)
+    base.after_save do |record|
+      record.readonly!
+    end
+
+    base.after_find do |record|
+      record.readonly!
+    end
+  end
 end
--- spec/active_record_immutable_spec.rb
+++ spec/active_record_immutable_spec.rb
@@ -3,13 +3,35 @@ require 'active_record_immutable'
 
 
 describe ActiveRecordImmutable do
-  let(:klass) do
-    klass = Temping.create :klass
-    klass.send(:extend, ActiveRecordImmutable)
+  Temping.create :klass do
+    with_columns do |t|
+      t.string :attr
+    end
+
+    extend ActiveRecordImmutable
   end
 
-  it 'is saveable after new' do
-    instance = klass.new
-    instance.save.should be_true
+  let(:instance) { Klass.create }
+
+  describe 'from saved' do
+    it 'is unsavable after save' do
+      expect{instance.save}.to raise_error(ActiveRecord::ReadOnlyRecord)
+    end
+
+    it 'is undestroyable after save' do
+      expect{instance.destroy}.to raise_error(ActiveRecord::ReadOnlyRecord)
+    end
+  end
+
+  describe 'from find' do
+    let(:from_db) { Klass.first }
+
+    it 'is unsavable from record find' do
+      expect{from_db.save}.to raise_error(ActiveRecord::ReadOnlyRecord)
+    end
+
+    it 'is undestroyable from record find' do
+      expect{from_db.destroy}.to raise_error(ActiveRecord::ReadOnlyRecord)
+    end
   end
 end

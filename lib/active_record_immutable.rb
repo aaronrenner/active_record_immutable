863702014bd6dc56058d8021272885a9070fe091
Aaron Renner <aaron@animascodelabs.com>
2014-12-19 10:12:40 -0700

    Raise error when accessing setters on immutable objects

--- lib/active_record_immutable.rb
+++ lib/active_record_immutable.rb
@@ -1,13 +1,25 @@
 require "active_record_immutable/version"
 
 module ActiveRecordImmutable
+  class ReadOnlyAttributeError < StandardError; end
+
   def self.extended(base)
-    base.after_save do |record|
+    make_immutable = lambda do |record|
       record.readonly!
+
+      record.attributes.each do |attr, _ |
+        record.define_singleton_method :"#{attr}=" do |_|
+          raise ReadOnlyAttributeError, ":#{attr} is read-only"
+        end
+      end
+    end
+
+    base.after_save do |record|
+      make_immutable.call(record)
     end
 
     base.after_find do |record|
-      record.readonly!
+      make_immutable.call(record)
     end
   end
 end
--- spec/active_record_immutable_spec.rb
+++ spec/active_record_immutable_spec.rb
@@ -13,6 +13,19 @@ describe ActiveRecordImmutable do
 
   let(:instance) { Klass.create }
 
+  describe 'when unsaved' do
+    it 'can be saved' do
+      klass = Klass.new
+      expect{ klass.save }.not_to raise_error
+    end
+
+    it 'can set attributes' do
+      klass = Klass.new
+      klass.attr = 'value'
+      expect(klass.attr).to eq('value')
+    end
+  end
+
   describe 'from saved' do
     it 'is unsavable after save' do
       expect{instance.save}.to raise_error(ActiveRecord::ReadOnlyRecord)
@@ -20,6 +33,10 @@ describe ActiveRecordImmutable do
 
     it 'is undestroyable after save' do
       expect{instance.destroy}.to raise_error(ActiveRecord::ReadOnlyRecord)
+    end
+
+    it 'raises an error if trying to set an attribute when immutable' do
+      expect{ instance.attr = 'value' }.to raise_error(ActiveRecordImmutable::ReadOnlyAttributeError, ":attr is read-only")
     end
   end
 
@@ -32,6 +49,10 @@ describe ActiveRecordImmutable do
 
     it 'is undestroyable from record find' do
       expect{from_db.destroy}.to raise_error(ActiveRecord::ReadOnlyRecord)
+    end
+
+    it 'raises an error if trying to set an attribute when immutable' do
+      expect{ from_db.attr = 'value' }.to raise_error(ActiveRecordImmutable::ReadOnlyAttributeError, ":attr is read-only")
     end
   end
 end
